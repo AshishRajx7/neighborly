76feb6ecb290c3ef422a43e6122feb27c222d51c
AshishRajx7 <ashishrajcr7@gmail.com>
2025-08-21 14:59:42 +0530

    Fix Expo CLI, reset node_modules, update dependencies for SDK compatibility

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -15,7 +15,7 @@ const firebaseConfig = {
   authDomain: FIREBASE_AUTH_DOMAIN,
   projectId: FIREBASE_PROJECT_ID,
   storageBucket: FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID, // ✅ fixed
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID, 
   appId: FIREBASE_APP_ID,
 };
 
